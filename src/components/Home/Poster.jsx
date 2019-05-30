e896304c2a8275aa3380d6224d2c5a6f0b470029
cherrih <cherri.hartigan@gmail.com>
2019-05-30 11:58:56 -0700

    Render canvas

--- src/components/Home/Poster.jsx
+++ src/components/Home/Poster.jsx
@@ -3,11 +3,18 @@ import React, { Component } from 'react';
 class Poster extends Component {
   constructor(props) {
     super(props);
+    this.state = {
+
+    };
+    this.canvasRef = React.createRef();
   }
+
   render() {
-    return(
-      <div>POSTER</div>
-    )
+    return (
+      <>
+        <canvas width="500" height="500" ref={this.canvasRef} />
+      </>
+    );
   }
 }
 
