1448daee6eb99bb11080d23c3e72d1e0dc7e363a
cherrih <cherri.hartigan@gmail.com>
2019-05-30 19:31:17 -0700

    Add form

--- src/components/Home/Cities.jsx
+++ src/components/Home/Cities.jsx
@@ -8,6 +8,18 @@ const Cities = (props) => {
         <div>Sign up to be the first to know when it launches!</div>
         <button type="button" className="cities-sign-up-button rounded-button">Sign Up</button>
       </div>
+      <form>
+        <input type="text" name="name" placeholder="I'm traveling to..." />
+        <input type="submit" value="Search" />
+      </form>
+      <div>
+        <div>
+          Coming soon...
+        </div>
+        <div className="cities-coming-soon-container">
+
+        </div>
+      </div>
     </div>
   );
 }
